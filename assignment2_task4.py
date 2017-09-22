47cd0dbb319b02d1e9bdf56ce61942fa2704278e
karthik burra <31553172+burrav93@users.noreply.github.com>
2017-09-21 20:28:33 -0500

    Delete assignment2_task4.py

--- assignment2_task4.py
+++
@@ -1,7 +0,0 @@
-import numpy as np         ##import numpy as np
-a = np.random.random(15)   ## 15 random numbers
-a[a.argmax()] = 100        ## this function searches for maximum value and replaces by 100
-print(a)
-
-
-
