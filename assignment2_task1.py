19e932cc531329fae88f86c2d42b3f3f569394c0
karthik burra <31553172+burrav93@users.noreply.github.com>
2017-09-21 20:27:45 -0500

    Delete assignment2_task1.py

--- assignment2_task1.py
+++
@@ -1,9 +0,0 @@
-
-string1= input("Enter a string:") ##**input to the user**##
-words=[word for word in string1.split(" ")] ##**it will split the string and remove the duplicate words**##
-result="".join(sorted(list(set(words))))##**set of words, then it will sort the split words and join them in order **##
-print(result)
-
-
-
-
